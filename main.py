7449229e4fe3078e70ae4d45e72d1843c6b67357
Султан Сайподинов <sultan05@bk.ru>
2025-01-04 17:07:56 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -89,6 +89,39 @@ def main():
     char_class = choice_char_class()
     print(start_training(char_name, char_class))
 
+# character_creation_module/main.py
 
-main()
+from random import randint
+
+# Новый импорт.
+# Из модуля start_game_banner, который расположен в папке graphic_arts,
+# импортируем функцию run_screensaver().
+from graphic_arts.start_game_banner import run_screensaver
+
+def attack(char_name: str, char_class: str) -> str:
+    ...
+
+def defence(char_name: str, char_class: str) -> str:
+    ...
+
+def special(char_name: str, char_class: str) -> str:
+    ...
+
+def start_training(char_name: str, char_class: str) -> str:
+    ...
+
+def choice_char_class() -> str:
+    ...
+
+if __name__ == '__main__':
+    run_screensaver()
+    print('Приветствую тебя, искатель приключений!')
+    print('Прежде чем начать игру...')
+    char_name: str = input('...назови себя: ')
+    print(f'Здравствуй, {char_name}! '
+          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
+    print('Ты можешь выбрать один из трёх путей силы:')
+    print('Воитель, Маг, Лекарь')
+    char_class: str = choice_char_class()
+    print(start_training(char_name, char_class)) 
 # комментарий
